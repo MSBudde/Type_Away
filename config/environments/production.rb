02b7d46fe9632de822d3b1269ccda11da545a089
Michael.s.Budde@gmail.com </ignore>
2016-07-28 20:23:07 -0700

    f

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -78,12 +78,12 @@ Rails.application.configure do
   config.active_record.dump_schema_after_migration = false
 
   config.paperclip_defaults = {
- :storage => :s3,
- :s3_credentials => {
-   access_key_id: ENV['AWS_ACCESS_KEY_ID'],
-   secret_acces_key: ENV['AWS_SECRET_ACCESS_KEY'],
-   s3_region: ENV['AWS_REGION'],
-   bucket: ENV['typeaway']
- }
-}
+      storage: :s3,
+      s3_host_name: 's3-us-west-2.amazonaws.com	',
+      s3_credentials: {
+        access_key_id: ENV['AWS_ACCESS_KEY_ID'],
+        secret_access_key: ENV['AWS_SECRET_ACCESS_KEY']
+      },
+      bucket: ENV['S3_BUCKET_NAME']
+  }
 end
