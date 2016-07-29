9d34c47d2718a3eccbd65652fef8ecfbdb10275c
Michael.s.Budde@gmail.com </ignore>
2016-07-28 19:18:02 -0700

    adding avatars

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -79,7 +79,6 @@ Rails.application.configure do
 
   config.paperclip_defaults = {
  :storage => :s3,
- s3_host_name: 'typeaway.s3-website-us-west-1.amazonaws.com',
  :s3_credentials => {
    access_key_id: ENV['AWS_ACCESS_KEY_ID'],
    secret_acces_key: ENV['AWS_SECRET_ACCESS_KEY'],
